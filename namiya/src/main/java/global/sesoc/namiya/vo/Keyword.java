885d14c32bdcb6ca4a8feed2b47c7bb1a71d43f6
SEHOON <SEHOON@LAPTOP-172OKBSG>
2018-09-30 17:59:27 +0900

    180930 - sh 키워드 vo 변경

--- namiya/src/main/java/global/sesoc/namiya/vo/Keyword.java
+++ namiya/src/main/java/global/sesoc/namiya/vo/Keyword.java
@@ -3,16 +3,18 @@ package global.sesoc.namiya.vo;
 public class Keyword {
 	private int keywordnum;
 	private String keywordname;
-	private String category;
+	private int categorynum;
 	private String userid;
 	
-	public Keyword() {}
+	public Keyword() {
+		// TODO Auto-generated constructor stub
+	}
 
-	public Keyword(int keywordnum, String keywordname, String category, String userid) {
+	public Keyword(int keywordnum, String keywordname, int categorynum, String userid) {
 		super();
 		this.keywordnum = keywordnum;
 		this.keywordname = keywordname;
-		this.category = category;
+		this.categorynum = categorynum;
 		this.userid = userid;
 	}
 
@@ -20,28 +22,28 @@ public class Keyword {
 		return keywordnum;
 	}
 
-	public void setKeywordnum(int keywordnum) {
-		this.keywordnum = keywordnum;
-	}
-
 	public String getKeywordname() {
 		return keywordname;
+	}
+
+	public int getCategorynum() {
+		return categorynum;
+	}
+
+	public String getUserid() {
+		return userid;
+	}
+
+	public void setKeywordnum(int keywordnum) {
+		this.keywordnum = keywordnum;
 	}
 
 	public void setKeywordname(String keywordname) {
 		this.keywordname = keywordname;
 	}
 
-	public String getCategory() {
-		return category;
-	}
-
-	public void setCategory(String category) {
-		this.category = category;
-	}
-
-	public String getUserid() {
-		return userid;
+	public void setCategorynum(int categorynum) {
+		this.categorynum = categorynum;
 	}
 
 	public void setUserid(String userid) {
@@ -50,10 +52,9 @@ public class Keyword {
 
 	@Override
 	public String toString() {
-		return "Keyword [keywordnum=" + keywordnum + ", keywordname=" + keywordname + ", category=" + category
+		return "Keyword [keywordnum=" + keywordnum + ", keywordname=" + keywordname + ", categorynum=" + categorynum
 				+ ", userid=" + userid + "]";
 	}
-	
 	
 	
 }
